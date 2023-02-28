c09afb1d95d584c4b030468d55329724480c4cb5
Oliver Lee <oliverlee@google.com>
2023-02-28 21:55:27 +0000

    Unify constructors fix and add test

--- core/src/main/java/org/apache/calcite/jdbc/Driver.java
+++ core/src/main/java/org/apache/calcite/jdbc/Driver.java
@@ -52,7 +52,7 @@ import java.util.Properties;
 public class Driver extends UnregisteredDriver {
   public static final String CONNECT_STRING_PREFIX = "jdbc:calcite:";
 
-  final Function0<CalcitePrepare> prepareFactory;
+  public final Function0<CalcitePrepare> prepareFactory;
 
   static {
     new Driver().register();
@@ -65,7 +65,7 @@ public class Driver extends UnregisteredDriver {
   }
 
   private Driver(Function0<CalcitePrepare> prepareFactory) {
-    super();
+    new Driver();
     this.prepareFactory = prepareFactory;
   }
 
