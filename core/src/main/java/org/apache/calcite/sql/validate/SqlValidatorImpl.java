5e071a5895ad0b520770abe077369cedf2816fa9
Mihai Budiu <mbudiu@feldera.com>
2024-11-06 16:41:42 -0800

    [CALCITE-6677] HAVING clauses fail validation when type coercion is applied to GROUP BY clause
    Signed-off-by: Mihai Budiu <mbudiu@feldera.com>

--- core/src/main/java/org/apache/calcite/sql/validate/SqlValidatorImpl.java
+++ core/src/main/java/org/apache/calcite/sql/validate/SqlValidatorImpl.java
@@ -4070,7 +4070,6 @@ public class SqlValidatorImpl implements SqlValidatorWithHints {
 
     validateWhereClause(select);
     validateGroupClause(select);
-    validateHavingClause(select);
     validateWindowClause(select);
     validateQualifyClause(select);
     handleOffsetFetch(select.getOffset(), select.getFetch());
@@ -4081,6 +4080,7 @@ public class SqlValidatorImpl implements SqlValidatorWithHints {
     final RelDataType rowType =
         validateSelectList(selectItems, select, targetRowType);
     ns.setType(rowType);
+    validateHavingClause(select);
 
     validateMustFilterRequirements(select, ns);
 
@@ -5018,8 +5018,8 @@ public class SqlValidatorImpl implements SqlValidatorWithHints {
     if (SqlUtil.containsCall(having, call -> call.getOperator() instanceof SqlOverOperator)) {
       throw newValidationError(originalHaving, RESOURCE.windowInHavingNotAllowed());
     }
-    havingScope.checkAggregateExpr(having, true);
     inferUnknownTypes(booleanType, havingScope, having);
+    havingScope.checkAggregateExpr(having, true);
     having.validate(this, havingScope);
     final RelDataType type = deriveType(havingScope, having);
     if (!SqlTypeUtil.inBooleanFamily(type)) {
